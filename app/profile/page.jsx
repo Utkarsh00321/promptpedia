02c2019f36726a3a9c839ea1f5e3a402d99bb21d
Utkarsh00321 <kadamutkarsh21@gmail.com>
2024-02-10 17:30:01 +0530

    fixed profile page isue

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,53 +1,61 @@
-'use client'
-import React from 'react'
-import { useState,useEffect } from 'react'
-import Profile from '@components/Profile'
-import { useSession } from 'next-auth/react'
-import { useRouter} from 'next/navigation'
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+import Profile from "@components/Profile";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const MyProfile = () => {
-   const {data:session} = useSession();
-   const [posts, setPosts] = useState([]);
-   const router = useRouter();
-   
-  const handleEdit = (post)=>{
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+  const router = useRouter();
+
+  const handleEdit = (post) => {
     router.push(`/update-prompt?id=${post._id}`);
-  }
+  };
 
-  const handleDelete = async (post)=>{
-    const hasConfirmed = confirm('Are you sure you want to delete this prompt ?');
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt ?"
+    );
 
-    if(hasConfirmed){
+    if (hasConfirmed) {
       try {
         await fetch(`/api/prompt/${post._id.toString()}`, {
           method: "DELETE",
         });
 
-        const filteredPosts = posts.filter((p)=>p._id!=post._id);
+        const filteredPosts = posts.filter((p) => p._id != post._id);
         setPosts(filteredPosts);
       } catch (error) {
         console.log(error);
       }
     }
-  }
+  };
 
   useEffect(() => {
-    const fetchPosts = async()=>{
+    const fetchPosts = async () => {
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
       const data = await response.json();
       setPosts(data);
-    }
-    if(session?.user.id)fetchPosts();
-  }, [])
+    };
+    if (session?.user.id) fetchPosts();
+  }, []);
   return (
-    <Profile
-     name="My"
-     desc ='Welcome to your profile!'
-     data = {posts}
-     handleEdit = {handleEdit}
-     handleDelete = {handleDelete}
-    />
-  )
-}
+    <>
+      {session ? (
+        <Profile
+          name="My"
+          desc="Welcome to your profile!"
+          data={posts}
+          handleEdit={handleEdit}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        <h1 className="text-4xl font-bold">Sign-in to see your profile ...</h1>
+      )}
+    </>
+  );
+};
 
-export default MyProfile
+export default MyProfile;
